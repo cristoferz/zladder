93d113a0b660242ab9d927a15ced58020873cd5b
Cristofer Zdepski <cristoferz@gmail.com>
2019-04-07 20:44:36 +0200

    Create folder structure for source codes (#4)
    Creates the folder structures for project source codes as related on issue #3.

---
+++ src/zladder/zladder.ino
@@ -0,0 +1,15 @@
+#include <EEPROM.h>
+
+void setup() {
+	// TODO
+	// Read Configuration from EEPROM and configure ports
+	// Read Program from EEPROM
+	
+}
+
+void loop() {
+	// TODO
+	// Read all inputs
+	// Run engine loop
+	// set outputs
+}
